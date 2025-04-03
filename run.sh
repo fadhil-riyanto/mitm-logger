febdc358eee5f4512c1c9d8aad8fb9133cf5f794
fadhil riyanto <me@fadev.org>
2025-04-03 13:10:35 +0700

    update run.sh, composer deps, and gitignore
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -a
 source .env
 set +a
 
-php -S 0.0.0.0:12343 &
+# php -S 0.0.0.0:12343 & -- this is deprecated now
+php -S 0.0.0.0:12343 ./http/server.php &
 mitmdump --mode regular@8081 -s hook.py
 wait
